0c9c27a714b8fb7a4e52d21c7220d85be308f1b5
Sergey-Alex <stroitelevsergeyalex@gmail.com>
2021-08-25 22:06:38 +0300

    create Item components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Counter from "./components/Counter";
+import './Styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-  return (
+  const [posts, setPosts] = useState([
+      {id: 1, title: 'JavaScript', body: 'Description'},
+      {id: 2, title: 'JavaScript 2', body: 'Description'},
+      {id: 3, title: 'JavaScript 3', body: 'Description'},
+      ])
+
+  const [posts2, setPosts2] = useState([
+      {id: 1, title: 'TypeScript', body: 'Description'},
+      {id: 2, title: 'TypeScript 2', body: 'Description'},
+      {id: 3, title: 'TypeScript 3', body: 'Description'},
+      ])
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostList posts={posts} title={'Посты про  JavaScript'}/>
+      <PostList posts={posts2} title={'Посты про TypeScript '}/>
     </div>
   );
 }
